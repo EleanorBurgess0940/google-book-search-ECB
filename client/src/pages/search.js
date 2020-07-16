83122404811c216e90988a3642dd51025f70d72d
Eleanor Burgess <eleanor.c.burgess@gmail.com>
2020-07-16 11:24:23 -0400

    added two pages of saved and searched and basic components of col container and row

---
+++ client/src/pages/search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function Search() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Search</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Search;

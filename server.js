64a09a8c79b71677a0cf8b37945d55d2f2c5a266
Eleanor Burgess <eleanor.c.burgess@gmail.com>
2020-07-16 10:51:32 -0400

    added routes folder and controllers folder using code from the Stu_AJAXBooks and will edit later

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +21,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-ongoose.connect(
+mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
 );
 
